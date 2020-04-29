7f5c2ce485824d9e94c3e719004251a3ccc29954
huangkai <420969651@qq.com>
2020-04-29 15:24:46 +0800

    spring boot 集成 druid mybatis

---
+++ 4-spring-boot-mybatis/src/main/java/com/mowen/filter/MybatisExampleApplication.java
@@ -0,0 +1,16 @@
+package com.mowen.filter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@MapperScan("com.mowen.mybatis")
+@SpringBootApplication
+public class MybatisExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisExampleApplication.class, args);
+    }
+}
