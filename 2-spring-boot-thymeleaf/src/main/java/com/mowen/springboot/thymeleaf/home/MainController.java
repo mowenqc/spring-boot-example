3c580f2878ed6cf348aea2dad3473f8db410093d
huangkai <420969651@qq.com>
2020-04-29 09:11:08 +0800

    thymeleaf project

--- 2-spring-boot-thymeleaf/src/main/java/com/mowen/springboot/thymeleaf/home/MainController.java
+++ 2-spring-boot-thymeleaf/src/main/java/com/mowen/springboot/thymeleaf/home/MainController.java
@@ -3,11 +3,14 @@ package com.mowen.springboot.thymeleaf.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.expression.Lists;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 /****
@@ -29,11 +32,42 @@ public class MainController {
             }
             modelMap.put("userName", userName);
             modelMap.put("date", new Date());
-            modelMap.put("count", new Random(10).nextInt());
+            List<User> userList = Arrays.asList(new User("m", 1), new User("o", 3));
+            modelMap.put("userList", userList);
+            modelMap.put("author", null);
+            modelMap.put("case", 1);
+            modelMap.put("email", "");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
         return "welcome";
+    }
+
+
+    public static class User{
+
+        private String name;
+        private int data;
+        public User(String name, int data){
+            this.name = name;
+            this.data = data;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
     }
 }
